5a3f18c151b9e9168d273e2f1d02b1f064ac13e8
Fisher <SwordFisherL42@gmail.com>
2023-03-28 16:10:50 -0400

    Add 'under construction' landing page

---
+++ under_construction/script.js
@@ -0,0 +1,6 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const chatNotificationAudio = document.getElementById("chat-notification");
+//     setTimeout(() => {
+//         chatNotificationAudio.play();
+//     }, 1500); // Play the sound after the same delay as the chat bubble animation (x seconds)
+// });
